da6b83f6c826cc4dafe3d2cdff43aff2cfd29b9a
ivan <lucky.boy.fot@gmail.com>
2021-12-07 09:22:34 +0200

    added copy

--- main.py
+++ main.py
@@ -3,36 +3,36 @@ import copy
 
 class Complex(object):
     def __init__(self, x):
-        self.x = complex(x)
+        self.__x = complex(x)
 
     def __add__(self, other):
-        return self.x - other.x
+        return self.__x - other.__x
 
     def __sub__(self, other):
-        return self.x - other.x
+        return self.__x - other.__x
 
     def __iadd__(self, other):
-        self.x = self + other
-        return self.x
+        self.__x = self + other
+        return self.__x
 
     def __isub__(self, other):
-        self.x = self - other
-        return self.x
+        self.__x = self - other
+        return self.__x
 
     def __mul__(self, other):
-        return self.x * other.x
+        return self.__x * other.__x
 
     def __truediv__(self, other):
-        return self.x / other.x
+        return self.__x / other.__x
 
     def __imul__(self, other):
-        self.x = self * other
-        return self.x
+        self.__x = self * other
+        return self.__x
 
     def __itruediv__(self, other):
-        self.x = self / other
-        return self.x
+        self.__x = self / other
+        return self.__x
 
     def __copy__(self):
         print("Копія")
-        return Complex(self.x)
+        return Complex(self.__x)
--- worker.py
+++ worker.py
@@ -1,28 +1,32 @@
 from main import Complex
 
-first_num = complex(input("Введіть перше число: "))
-operator = input("Введіть знак: ")
-second_num = complex(input("Введіть друге число: "))
-number1 = Complex(first_num)
-number2 = Complex(second_num)
+def main():
+    first_num = complex(input("Введіть перше число: "))
+    operator = input("Введіть знак: ")
+    second_num = complex(input("Введіть друге число: "))
+    number1 = Complex(first_num)
+    number2 = Complex(second_num)
 
-if operator == '+':
-    print(number1 + number2)
-elif operator == '-':
-    print(number1 - number2)
-elif operator == '*':
-    print(number1 * number2)
-elif operator == '/':
-    print(number1 / number2)
-elif operator == '+=':
-    number1 += number2
-    print(number1)
-elif operator == '-=':
-    number1 -= number2
-    print(number1)
-elif operator == '*=':
-    number1 *= number2
-    print(number1)
-elif operator == '/=':
-    number1 /= number2
-    print(number1)
+    if operator == '+':
+        print(number1 + number2)
+    elif operator == '-':
+        print(number1 - number2)
+    elif operator == '*':
+        print(number1 * number2)
+    elif operator == '/':
+        print(number1 / number2)
+    elif operator == '+=':
+        number1 += number2
+        print(number1)
+    elif operator == '-=':
+        number1 -= number2
+        print(number1)
+    elif operator == '*=':
+        number1 *= number2
+        print(number1)
+    elif operator == '/=':
+        number1 /= number2
+        print(number1)
+
+
+main()
